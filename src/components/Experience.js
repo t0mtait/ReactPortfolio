7bb585f679df9f6d2952d679a750eb8c2b0be856
tom tait <thomas.t.ca@outlook.com>
2025-06-20 19:43:54 -0300

    more styling upgrades

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,7 +1,80 @@
+import { useMediaQuery } from 'react-responsive'
+
 function Experience() {
-    return (
-        <section class="bg-transparent" id = "experience">
-            <h1 className="mt-2 mb-4 text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Experience</h1>
+    const isMobile = useMediaQuery({ maxWidth: 600})
+
+    if (isMobile)
+    {
+        return (
+                <section class="bg-transparent" id = "experience">
+            <h1 className="mt-2 mb-4 text-4xl text-center font-extrabold leading-none tracking-tight text-yellow-300 md:text-5xl lg:text-6xl">Experience</h1>
+            <div
+                className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+
+                <div className="mt-4 md:mt-0">
+                    
+                    <h2 className="mb-1 text-xl tracking-tight font-extrabold text-white ">VeroSource</h2>
+                    <h2 className="mb-1 text-m tracking-tight font-extrabold text-white ">Software Developer</h2>
+                    <h2 className="mb-4 text-sm tracking-tight font-extrabold text-white ">January 2025 - Present</h2>
+                    <ul className="max-w-md space-y-1 text-white list-disc list-inside ">
+
+                    </ul>
+
+                </div>
+                <img className="w-1/2 hidden dark:block" src="verosource.png" alt="dashboard image"/>
+            </div>
+            <div
+                className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+                
+                <div className="mt-4 md:mt-0">
+                    <h2 className="mb-1 text-xl tracking-tight font-extrabold text-white ">Sonrai Security</h2>
+                    <h2 className="mb-1 text-m tracking-tight font-extrabold text-white ">Software Developer</h2>
+                    <h2 className="mb-4 text-sm tracking-tight font-extrabold text-white ">January 2024 - August 2024</h2>
+                    <ul className="max-w-md space-y-1 text-white text-xs list-disc list-inside ">
+                        <li>
+                            Developed and maintained a cloud security platform.
+                        </li>
+                        <li>
+                            Wrote end to end tests in JavaScript for our webapp.
+                        </li>
+                        <li>
+                            Built a script to destroy resources every night to cut our AWS, Azure, and
+                            GCP costs.
+                        </li>
+                    </ul>
+                </div>
+                <img className="mt-5 w-1/2 hidden dark:block" src="sonrai.png" alt="dashboard image"/>
+            </div>
+
+            <div
+                className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+                <div className="mt-4 md:mt-0">
+                    <h2 className="mb-1 text-xl tracking-tight font-extrabold text-white ">Irving Oil</h2>
+                    <h2 className="mb-1 text-m tracking-tight font-extrabold text-white ">Software Developer</h2>
+                    <h2 className="mb-4 text-sm tracking-tight font-extrabold text-white ">May 2023 - August 2023</h2>
+                    <ul className="max-w-md space-y-1 text-white text-xs list-disc list-inside ">
+                        <li>
+                            Developed and maintained a C# Rest API.
+                        </li>
+                        <li>
+                            Created and optimized SQL databases.
+                        </li>
+                        <li>
+                            Created a new customer service workflow using AWS.
+                        </li>
+                    </ul>
+
+                </div>
+                <img className="mt-4 w-2/5 hidden dark:block" src="irving.png" alt="dashboard image"/>
+            </div>
+        </section>
+    )
+    }
+    else
+    {
+        return (
+                <section class="bg-transparent" id = "experience">
+            <h1 className="mt-2 mb-4 text-4xl text-center font-extrabold leading-none tracking-tight text-yellow-300 md:text-5xl lg:text-6xl">Experience</h1>
             <div
                 className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
 
@@ -63,6 +136,8 @@ function Experience() {
             </div>
         </section>
     )
+    }
+    
 }
 
 export default Experience;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,62 @@
-
+import { useMediaQuery } from 'react-responsive'
 
 
 function Projects () {
-    return (
-
+    const isMobile = useMediaQuery({ maxWidth: 600})
+    if (isMobile) {
+        return (
         <section id = "projects">
-            <h1 className="mb-4 text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Projects</h1>
+            <h1 className="mb-4 mt-6 text-4xl text-center font-extrabold leading-none tracking-tight text-yellow-300 md:text-5xl lg:text-6xl">Projects</h1>
+            <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+                <div className="mt-4 md:mt-0">
+                    <h2 className="mb-1 text-4xl tracking-tight font-extrabold text-white ">CryptoPort</h2>
+                    <img className="w-full hidden mt-5 border-2 rounded-xl dark:block" src="cryptoport.png" alt="dashboard image"/>
+                    <ul className="max-w-md mt-5 space-y-1 text-white list-disc list-inside ">
+                        <li>
+                            WebApp created using EJS.
+                        </li>
+                        <li>
+                            Monitor your crypto investments in real time.
+                        </li>
+                        <li>
+                            AWS DynamoDB and EC2.
+                        </li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+            <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+                <div className="mt-4 md:mt-0">
+                    <h2 className="mb-1 text-4xl tracking-tight font-extrabold text-white ">My Portfolio</h2>
+                    <img className="w-full hidden mt-5 border-2 rounded-xl dark:block" src="portfolio.png" alt="dashboard image"/>
+                    <ul className="max-w-md mt-5 space-y-1 text-white list-disc list-inside ">
+                        <li>
+                            Built with React & Tailwind.
+                        </li>
+                        <li>
+                            Updated regularly.
+                        </li>
+                        <li>
+                            Showcases my experience and projects.
+                        </li>
+                    </ul>
+
+                </div>
+
+            </div>
+        </section>
+    )
+    }
+    else {
+        return (
+        <section id = "projects">
+            <h1 className="mb-4 mt-6 text-4xl text-center font-extrabold leading-none tracking-tight text-yellow-300 md:text-5xl lg:text-6xl">Projects</h1>
             <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
                 <img className="w-full hidden  border-2 rounded-xl dark:block" src="cryptoport.png" alt="dashboard image"/>
                 <div className="mt-4 md:mt-0">
-                    <h2 className="mb-1 text-4xl tracking-tight font-extrabold text-white ">CryptoPort</h2>
-                    <h2 className="mb-1 text-2xl tracking-tight font-extrabold text-white ">WebApp</h2>
+                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white ">CryptoPort</h2>
                     <ul className="max-w-md space-y-1 text-white list-disc list-inside ">
                         <li>
                             Created using EJS.
@@ -26,8 +72,30 @@ function Projects () {
                 </div>
 
             </div>
+
+            <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+                <img className="w-full hidden  border-2 rounded-xl dark:block" src="portfolio.png" alt="dashboard image"/>
+                <div className="mt-4 md:mt-0">
+                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white ">My Portfolio</h2>
+                    <ul className="max-w-md space-y-1 text-white list-disc list-inside ">
+                        <li>
+                            Built with React & Tailwind.
+                        </li>
+                        <li>
+                            Updated regularly.
+                        </li>
+                        <li>
+                            Showcases my experience and projects.
+                        </li>
+                    </ul>
+
+                </div>
+
+            </div>
         </section>
     )
+    }
+    
 }
 
 export default Projects
